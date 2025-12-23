a60e75dc72a8cc827e5a57f32223e5ce9940e597
pmlchamodya <lakinduchamodya03@gmail.com>
2025-12-23 18:11:44 +0530

    added  socket io notication

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import http from "http";
+import { Server } from "socket.io";
 import connectDB from "./config/db.js";
+
+// Routes Imports
 import authRoutes from "./routes/auth.js";
 import adminRoutes from "./routes/admin.js";
 import userRoutes from "./routes/user.js";
@@ -10,13 +14,39 @@ import inquiryRoutes from "./routes/inquiry.js";
 import roomRoutes from "./routes/rooms.js";
 import facilityRoutes from "./routes/facilities.js";
 
-
 dotenv.config();
 
 // Connect to MongoDB
 connectDB();
 
 const app = express();
+
+// --- SOCKET.IO
+// Create a standard HTTP server using Express app
+const server = http.createServer(app);
+
+// Initialize Socket.io with CORS settings
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  },
+});
+
+// Run when a client connects
+io.on("connection", (socket) => {
+  console.log(`⚡ User Connected: ${socket.id}`);
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+// Middleware to Attach 'io' to every request
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
 
 // Middleware
 app.use(cors());
@@ -39,6 +69,5 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
+// IMPORTANT: Use server.listen instead of app.listen
+server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
